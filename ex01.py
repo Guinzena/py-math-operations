8fdcb73c12eb555a2804f844ebea6c04977e5567
Guilherme Rodrigues <124007099+Guinzena@users.noreply.github.com>
2023-02-02 14:33:46 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,8 +1,8 @@
 # Ex01: Imprimir o sucessor e antecessor de um número inteiro
-num = int(input('Olá! Me diga qualquer número inteiro: '))
+num = int(input('Hi! Tell me any integer number: '))
 # Dá valor ao sucessor e antecessor
 suces = num + 1
-antec = num - 1
-print('Analizando o número {}, podemos dizer que...\n'
-      'O seu sucessor é {} \n'
-      'E seu antecessor é {} \n' .format(num, suces, antec))
+predec = num - 1
+print('Analyzing your number {}, we can say that...\n'
+      'The sucessor is {} \n'
+      'And the predecessor is {}' .format(num, suces, predec))

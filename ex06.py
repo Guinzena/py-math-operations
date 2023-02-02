53894bf75aa55cd250db61210f07d60651f1d10e
Guilherme Rodrigues <124007099+Guinzena@users.noreply.github.com>
2023-02-02 15:07:37 -0300

    Update ex06.py

--- ex06.py
+++ ex06.py
@@ -1,10 +1,10 @@
 # Ex06: Calcule a área de uma parede e responda quantos litros de tinta serão necessários
 # Cada litro de tinta pinta 2m² de parede
-alt = float(input('Bem Vindo(a)! Vamos calcular quantos litros de tinta lhe serão necessários \n'
-                  'Qual é a altura(m) da parede que você deseja pintar? '))
-larg = float(input('E qual é a medida da largura(m)? '))
+height = float(input('Welcome! Lets calculate how many liters of ink will be necessary \n'
+                  'What is the height(m) of the wall? '))
+width = float(input('And what is the width(m)? '))
 # Faz o cálculo da área e em seguida do núemro de litros necessários
-area = alt * larg
-litros = area / 2
-print('Com os respectivos valores({}, {}), sua parede tem uma área de {}m² \n'
-      'Sendo assim necessário {}l de tinta para pinta-la.'.format(alt, larg, area, litros))
+area = height * width
+liters = area / 2
+print('With the respective values({}, {}), your wall has an area of {}m² \n'
+      'Being necessary {}l of ink to paint it.'.format(height, width, area, liters))

0d2f9100d8c775c261cbabddb60452ddfb537d7a
Guilherme Rodrigues <124007099+Guinzena@users.noreply.github.com>
2023-02-02 15:20:18 -0300

    Update ex08.py

--- ex08.py
+++ ex08.py
@@ -1,12 +1,12 @@
 # ex08: Receba duas notas de um aluno e calcule sua média
-nome = str(input('Bom dia! \n'
-                    'Digite o nome do aluno(a): '))
-nota1 = float(input('Agora digite a sua 1° nota: '))
-nota2 = float(input('E agora a 2° nota: '))
+name = str(input('Good morning! \n'
+                    'Enter the students name: '))
+grade1 = float(input('Now enter the 1st grade: '))
+grade2 = float(input('And now 2nd grade: '))
 # Faz o cálculo da média
-media = (nota1 + nota2) / 2
+average = (grade1 + grade2) / 2
 # Utilizando o \033 para alterar as coresdo terminal
-print('\033[32mBoletim de {}\033[m \n'
-      '1° Prova: \033[32m{}\033[m \n'
-      '2° Prova: \033[32m{}\033[m \n'
-      'Média: \033[32m{}\033[m'.format(nome, nota1, nota2, media))
+print('\033[32mBulletin of {}\033[m \n'
+      '1st Test: \033[32m{}\033[m \n'
+      '2nd Test: \033[32m{}\033[m \n'
+      'Average: \033[32m{:.1f}\033[m'.format(name, grade1, grade2, average))

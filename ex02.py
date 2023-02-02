bfb577f6450177b3f980e4e8280bd72da677c1f1
Guilherme Rodrigues <124007099+Guinzena@users.noreply.github.com>
2023-02-02 14:46:25 -0300

    Update ex02.py

--- ex02.py
+++ ex02.py
@@ -1,11 +1,11 @@
 # Ex02: Ler um número e imprimir respecticamente seu dobro, triplo  e raíz quadrada
-num = int(input('Olá! Preciso fazer cálculos! Me diga um número: '))
+num = int(input('Hi! I need to do operations! Tell me a number: '))
 # Realiza os cálculos
-dob = num * 2
+doub = num * 2
 tri = num * 3
 # Potencializa o valor 0.5 para forçar uma raíz quadrada
-raiz = num ** (1/2)
-print('Quanto ao número {} posso dizer que... \n'
-      'O valor do seu dobro é {} \n'
-      'Seu triplo resulta em {} \n'
-      'E sua raíz quadrada é {} \n'.format(num, dob, tri, raiz))
+root = num ** (1/2)
+print('About your number {} i can say... \n'
+      'The value of its double is {} \n'
+      'Its triple results in {} \n'
+      'And its square root is {} \n'.format(num, doub, tri, root))

7261f369d2a3abf41502829b0644a438403519b0
Guilherme Rodrigues <124007099+Guinzena@users.noreply.github.com>
2023-02-02 14:50:16 -0300

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,8 +1,8 @@
 # Ex03: Ler um valor em metros e mostrar sua conversão em quilômetros, centímetros e milímetros
-m = int(input('Bem-vindo(a) ao conversor de medidas, digite seu valor em metros: '))
+m = int(input('Welcome to the measurement converter, enter your value in meters: '))
 # Converte para os respectivos valores
 km = m / 1000
 cm = m * 100
 mm = m * 1000
-print('O seu valor de {} metros resulta em: \n'
-      '{} quilômetros, {} centímetros, {} milímetros'.format(m, km, cm, mm))
+print('Your value of {} meters results in: \n'
+      '{} kilometers, {} centimeters and {} millimeters'.format(m, km, cm, mm))
